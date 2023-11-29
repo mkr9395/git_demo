99256aeac8895147f1eb55d30fb51e7d1d70e009
Mohit Kumar <mkr9395@gmail.com>
2023-11-29 13:24:16 +0530

    "2 lib added"

--- predict_pipeline.py
+++ predict_pipeline.py
@@ -3,6 +3,26 @@ import os
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class LitModule(pl.LightningModule):
+    def __init__(self, layer_1_dim=128, learning_rate=1e-2):
+        super().__init__()
+        # layer_1_dim and learning_rate will be logged by DVCLive
+        self.save_hyperparameters()
+
+    def training_step(self, batch, batch_idx):
+        metric = ...
+        # See Output Format bellow
+        self.log("train_metric", metric, on_step=False, on_epoch=True)
+
+dvclive_logger = DVCLiveLogger(save_dvc_exp=True)
+
+model = LitModule()
+trainer = pl.Trainer(logger=dvclive_logger)
+trainer.fit(model)
+
 
 from src.exception import CustomException
 from src.utils import load_object
